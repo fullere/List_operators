a7c66df1a3673b6d4e8f6a16d9f596d72c16b89f
fullere <fullere@hartwick.edu>
2020-02-10 15:25:24 -0500

    saving start of project before laptop dies. created names of methods and started user interface

---
+++ Interface.py
@@ -0,0 +1,5 @@
+# Elizabeth Fuller
+# 2/10/2020
+# list operators interface. inputs and outputs
+
+print("Please enter five values. Use numerals only.")
--- Operations.py
+++ Operations.py
@@ -1,1 +1,16 @@
 # Elizabeth Fuller
+# 2/10/2020
+# List operators methods
+
+def sort_list():
+def sum_of_list():
+def product():
+def mean():
+def median():
+def mode():
+def largest():
+def smallest():
+def no_dup():
+def no_even():
+def no_odd():
+def look_for():
