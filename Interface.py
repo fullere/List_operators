90eabe363674c3f2b4afe287b5335a8757db1e89
fullere <fullere@hartwick.edu>
2020-02-11 10:00:31 -0500

    finished

--- Interface.py
+++ Interface.py
@@ -2,4 +2,67 @@
 # 2/10/2020
 # list operators interface. inputs and outputs
 
-print("Please enter five values. Use numerals only.")
+import Operations
+
+lists = []
+print("Please enter a series of five values. Use numerals only.")
+first = input("First number: ")
+first = int(first)
+lists.append(first)
+second = input("Second number: ")
+second = int(second)
+lists.append(second)
+third = input("Third number: ")
+third = int(third)
+lists.append(third)
+fourth = input("Fourth number: ")
+fourth = int(fourth)
+lists.append(fourth)
+fifth = input("Fifth number: ")
+fifth = int(fifth)
+lists.append(fifth)
+
+print("This is your list sorted smallest to largest:")
+print(Operations.sort_list(lists))
+
+print("This is the sum of your list:")
+print(Operations.sum_of_list(lists))
+
+print("This is the product of your list:")
+print(Operations.product(lists))
+
+print('This is the average of your list:')
+print(Operations.mean(lists))
+
+print("This is the median of your list:")
+print(Operations.median(lists))
+
+print("This is the mode of your list:")
+print(Operations.modes(lists))
+
+print("This is the largest number in your list:")
+print(Operations.largest(lists))
+
+print("This is the second largest number in the list:")
+print(Operations.second_largest(lists))
+
+print("This is the smallest number in your list:")
+print(Operations.smallest(lists))
+
+print("This is your list without duplicates:")
+print(Operations.no_dups(lists))
+
+print("This is your list without even numbers:")
+print(Operations.no_even(lists))
+
+print("This is your list without odd numbers:")
+print(Operations.no_odd(lists))
+
+print("Enter a number to see if it is in your list: ")
+looking = input()
+looking = int(looking)
+found = Operations.look_for(lists, looking)
+if found == True:
+    print(f"{looking} is in your list.")
+else:
+    print(f"{looking} is not in your list.")
--- Operations.py
+++ Operations.py
@@ -1,16 +1,117 @@
 # Elizabeth Fuller
 # 2/10/2020
-# List operators methods
+# List operators methods. uses 5 numbers in a list
 
-def sort_list():
-def sum_of_list():
-def product():
-def mean():
-def median():
-def mode():
-def largest():
-def smallest():
-def no_dup():
-def no_even():
-def no_odd():
-def look_for():
+import collections
+
+def sort_list(list):
+    return sorted(list)
+
+
+def sum_of_list(list):
+    # adds the numbers in the list together
+    total = 0
+    for num in list:
+        total = total + num
+    return total
+
+
+def product(list):
+    # multiplies the numbers in the list together
+    total = 1
+    for num in list:
+        total = total * num
+    return total
+
+
+def mean(list):
+    # finds the average of the list
+    i = 1
+    total = 0
+    for num in list:
+        total = total + num
+        i = i + 1
+    average = total/i
+    return average
+
+
+def median(list):
+    # finds the median of a list of 5 numbers
+    sort = sorted(list)
+    medi = sort[2]
+    return medi
+
+
+def modes(numbers):
+    # finds the mode of the list of numbers
+    data = collections.Counter(numbers)
+    data_list = dict(data)
+    max_value = max(list(data.values()))
+    mode_val = [num for num, freq in data_list.items() if freq == max_value]
+    if len(mode_val) == len(numbers):
+        return "No mode"
+    else:
+        return mode_val
+
+
+def largest(list):
+    # find the largest number
+    lar = 0
+    for num in list:
+        if num > lar:
+            lar = num
+    return lar
+
+
+def smallest(list):
+    # find the smallest number
+    small = list[0]
+    for num in list:
+        if num < small:
+            small = num
+    return small
+
+
+def no_dups(list):
+    # creates a new list without duplicates
+    list3 = sorted(list)
+    list4 = []
+    [list4.append(x) for x in list3 if x not in list4]
+    return list4
+
+
+def no_even(list):
+    # creates new list without evens
+    list_odd = []
+    for num in list:
+        if num % 2 != 0:
+            list_odd.append(num)
+
+    return list_odd
+
+
+def no_odd(list):
+    # creates new list without odds
+    list_eve = []
+    for num in list:
+        if num % 2 == 0:
+            list_eve.append(num)
+
+    return list_eve
+    return
+
+
+def look_for(list, search):
+    # look for a number in the list
+    for num in list:
+        if search == num:
+            return True
+        else:
+            return False
+    return
+
+
+def second_largest(list):
+    # find the second largest number
+    sort = sorted(list)
+    return sort[-2]
